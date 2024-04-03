74ab8c7649dec1a46ea394cde1a0cce9016cea0f
ChanghyukWoo <changhyuk@uchanghyeog-ui-MacBookPro.local>
2024-04-03 16:41:16 +0900

    feat: auth api는 jwt 검증 filter 통과하지 않도록 변경

--- src/main/kotlin/pmeet/pmeetserver/config/SecurityConfig.kt
+++ src/main/kotlin/pmeet/pmeetserver/config/SecurityConfig.kt
@@ -2,6 +2,8 @@ package pmeet.pmeetserver.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
@@ -11,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter
 import org.springframework.security.web.server.authentication.ServerAuthenticationFailureHandler
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers
 
 @EnableWebFluxSecurity
@@ -19,6 +22,21 @@ class SecurityConfig {
   @Bean
   fun passwordEncoder(): PasswordEncoder {
     return BCryptPasswordEncoder()
+  }
+
+  @Order(Ordered.HIGHEST_PRECEDENCE)
+  @Bean
+  open fun authApiFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+    return http {
+      cors { disable() }
+      csrf { disable() }
+      formLogin { disable() }
+      httpBasic { disable() }
+      securityMatcher(PathPatternParserServerWebExchangeMatcher("/api/v1/auth/**"))
+      authorizeExchange {
+        authorize("/api/v1/auth/**", permitAll)
+      }
+    }
   }
 
   @Bean
@@ -39,7 +57,6 @@ class SecurityConfig {
       httpBasic { disable() }
       authorizeExchange {
         authorize(pathMatchers("/v3/api-docs/**", "/webjars/**"), permitAll)
-        authorize("/api/v1/auth/**", permitAll)
         authorize(anyExchange, authenticated)
       }
       addFilterAt(filter, SecurityWebFiltersOrder.AUTHENTICATION)
