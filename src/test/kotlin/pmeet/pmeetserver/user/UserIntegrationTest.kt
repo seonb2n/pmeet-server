6f5580558e148beae322a143d56fa581993bbee5
seonb2n <626ksb@gmail.com>
2024-06-15 14:02:07 +0900

    auto test

--- src/test/kotlin/pmeet/pmeetserver/user/UserIntegrationTest.kt
+++ src/test/kotlin/pmeet/pmeetserver/user/UserIntegrationTest.kt
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockAuthentication
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
@@ -32,6 +33,7 @@ import java.time.LocalDate
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @ExperimentalCoroutinesApi
+@ActiveProfiles("test")
 internal class UserIntegrationTest : DescribeSpec() {
   companion object {
     @Container
